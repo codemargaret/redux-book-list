ad41b44dcbd3b07ea09e205296766736b5f23cf4
Margaret Berry <codeberry1@gmail.com>
2018-02-04 15:33:12 -0800

    show book list and details on click

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript: the Good Parts', pages: 101 },
+    { title: 'Harry Potter and Order of the Phoenix', pages: 1000 },
+    { title: 'The Dark Tower', pages: 85 },
+    { title: 'Eloquent Ruby', pages: 1 }
+  ];
+}
